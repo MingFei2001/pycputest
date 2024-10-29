ecac439414ea651bf89ce0cfbf4cca0358000220
MingFei <108344660+MingFei2001@users.noreply.github.com>
2024-10-30 06:03:39 +0800

    added some debug print function

--- test.py
+++ test.py
@@ -1,45 +1,70 @@
+####################################################
+#  ____         ____ ____  _   _ _____         _
+# |  _ \ _   _ / ___|  _ \| | | |_   _|__  ___| |_
+# | |_) | | | | |   | |_) | | | | | |/ _ \/ __| __|
+# |  __/| |_| | |___|  __/| |_| | | |  __/\__ \ |_
+# |_|    \__, |\____|_|    \___/  |_|\___||___/\__|
+#        |___/
+####################################################
+# made by mingfei & LM studio
+####################################################
+
+# importing libraries
 import multiprocessing
 import time
 import numpy as np
 
 
-def cpu_intensive_task(index, start_time):
+# stress testing function
+def cpu_intensive_task(index, start_time, test_time):
     print(f"Process {index} started.")
-    while (time.perf_counter() - start_time) < 60:  # Run for 1 minute
+    while (time.perf_counter() - start_time) < test_time:
         try:
+            # get a random number & calculate sin, cos, tan of the said number
             x = np.random.rand(10000000)
             y = np.sin(x)
             z = np.cos(y)
             w = np.tan(z)
-            print(x + y + z + w)
+            print(f"random: {x}")
         except Exception as e:
+            # error handling
             print(f"Error in process {index}: {str(e)}")
+            print("Program exiting...")
         finally:
             # release any resources used by the process
-            print("Process ended.")
             pass
 
 
+# main function
 def main():
     num_physical_cores = multiprocessing.cpu_count()
-    print("CPU Stressing Tool started. Press Ctrl+C to stop.")
-    print(f"Running on {
-          num_physical_cores} physical cores and potentially more logical cores.")
+    # get test time from user
+    try:
+        test_time = int(input("How long you want this test to run:"))
+    except Exception:
+        # error handling
+        print("The value is not a integer.")
+        print("Program exiting...")
+        exit()
 
+    # stdout message
+    print("CPU Stressing Tool started. Press Ctrl+C to stop.")
+    print(f"Running for {test_time} second(s) on {
+          num_physical_cores} physical cores.")
+
+    # timer
     start_time_all = time.perf_counter()
 
+    # spawn processes to use all cores
     processes = []
     for i in range(num_physical_cores):
         index = i
         p = multiprocessing.Process(
-            target=cpu_intensive_task, args=(index, start_time_all))
+            target=cpu_intensive_task, args=(index, start_time_all, test_time))
         p.start()
         processes.append(p)
 
-    while True:
-        # do nothing, let the processes run indefinitely
-        pass
 
-
+# run only if not being imported by other program
 if __name__ == "__main__":
     main()
